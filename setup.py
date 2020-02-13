88a34d473a78085d6df5d03537985e200b052f2f
Andrey Kislyuk <kislyuk@gmail.com>
2020-02-13 10:38:47 -0800

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="miniwdl-s3parcp",
-    version="0.0.1",
+    version="0.0.2",
     description="miniwdl download plugin for s3:// using s3parcp",
     author="Mike Lin, Andrey Kislyuk",
     py_modules=["miniwdl_s3parcp"],
