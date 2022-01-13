daa826ef09cdce84479fa2dad19413122f8e3c45
morsecodist <todd@morsecodist.io>
2022-01-12 22:48:49 -0500

    s3parcp url

--- sfn-wdl/sfnwdl_miniwdl_plugin.py
+++ sfn-wdl/sfnwdl_miniwdl_plugin.py
@@ -87,6 +87,7 @@ def task(cfg, logger, run_id, run_dir, task, **recv):
         network = urllib.parse.urlparse(os.environ["AWS_ENDPOINT_URL"]).hostname
         recv["container"].create_service_kwargs["networks"] = [network]
         recv["env"].append(f"AWS_ENDPOINT_URL={os.environ['AWS_ENDPOINT_URL']}")
+        recv["env"].append(f"S3PARCP_S3_URL={os.environ['AWS_ENDPOINT_URL']}")
 
     # inject command to log `aws sts get-caller-identity` to confirm AWS_CONTAINER_CREDENTIALS_RELATIVE_URI
     # is passed through & effective
