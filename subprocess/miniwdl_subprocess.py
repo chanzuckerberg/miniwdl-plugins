c48e83fdec49481abad1052ea35b439bf36639bb
bernd.hoehl@monotype.com <x2n+M?PP?G:[>
2023-10-05 08:48:13 +0200

    fix cli name

--- subprocess/miniwdl_subprocess.py
+++ subprocess/miniwdl_subprocess.py
@@ -26,7 +26,7 @@ class LocalSubprocess(SubprocessBase):
 
     @property
     def cli_name(self) -> str:
-        return "local"
+        return "subprocess
 
     @property
     def cli_exe(self) -> List[str]:
