6f036787ba420e52e78db7439d6d4d8b55a520df
bernd.hoehl@monotype.com <x2n+M?PP?G:[>
2023-10-05 07:59:03 +0200

    adding support for local subprocess backend

---
+++ subprocess/miniwdl_subprocess.py
@@ -0,0 +1,44 @@
+# NOTE: this file is excluded from coverage analysis since alternate container backends may not be
+#       available in the CI environment. To test locally: prove -v tests/singularity.t
+import os
+import logging
+from typing import List
+from contextlib import ExitStack
+from WDL.runtime import config
+from WDL.runtime.backend.cli_subprocess import SubprocessBase
+from typing import ContextManager
+
+
+class LocalSubprocess(SubprocessBase):
+    """
+    local task runtime based on cli_subprocess.SubprocessBase
+    """
+
+    @classmethod
+    def global_init(cls, cfg: config.Loader, logger: logging.Logger) -> None:
+        cfg.override({"file_io": {"copy_input_files": True}})
+        logger.notice("Local runtime initialized (BETA)")
+
+    def __init__(self, cfg: config.Loader, run_id: str, host_dir: str) -> None:
+        super().__init__(cfg, run_id, host_dir)
+        self.container_dir = self.host_dir
+        # self.host_dir = os.path.join(self.host_dir, "work")
+
+    @property
+    def cli_name(self) -> str:
+        return "local"
+
+    @property
+    def cli_exe(self) -> List[str]:
+        return self.cfg.get_list("singularity", "exe")
+
+    def _pull(self, logger: logging.Logger, cleanup: ExitStack) -> str:
+        pass
+
+    def _run_invocation(self, logger: logging.Logger, cleanup: ExitStack, image: str) -> List[str]:
+        self.host_dir = os.path.join(self.host_dir, "work")
+        return []
+
+    def task_running_context(self) -> ContextManager[None]:
+        self.host_dir = self.container_dir
+        return super().task_running_context()
---
+++ subprocess/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(path.dirname(__file__), "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="local-miniwdl-plugin",
+    version="0.0.1",
+    description="miniwdl plugin for running tasks locally without using containers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Bernd Höhl",
+    py_modules=["miniwdl_subprocess"],
+    python_requires=">=3.6",
+    setup_requires=["reentry"],
+    install_requires=[],
+    reentry_register=True,
+    entry_points={
+        "miniwdl.plugin.container_backend": ["local = miniwdl_subprocess:LocalSubprocess"]
+    },
+)
